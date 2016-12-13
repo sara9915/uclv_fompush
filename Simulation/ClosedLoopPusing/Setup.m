a470e37e51c5546d67f3fc0b17ceefe7d83f328c
eudaldrg <eudald.rg@gmail.com>
2016-12-13 02:04:22 -0500

    Implemented choosing the right setup depending on OS

--- Simulation/ClosedLoopPusing/Setup.m
+++ Simulation/ClosedLoopPusing/Setup.m
@@ -1,3 +1,8 @@
 addpath('../../software/externals/jsonlab');
-addpath('/home/mcube/software/gurobi651/linux64/matlab');
-gurobi_setup;
+if isunix()
+    addpath('/home/mcube/software/gurobi651/linux64/matlab');
+    gurobi_setup;
+elseif ispc()
+    addpath('C:\gurobi701\win64\matlab');
+end
+    
