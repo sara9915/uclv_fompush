44a4fb71789f4dc15068ff128f51813680cd4cfd
eudaldrg <eudald.rg@gmail.com>
2016-12-13 02:03:43 -0500

    Changed default drawing color to blue

--- Simulation/ClosedLoopPusing/DrawTrajectoryOnImage.m
+++ Simulation/ClosedLoopPusing/DrawTrajectoryOnImage.m
@@ -1,9 +1,9 @@
 function [drawn_image] = DrawTrajectoryOnImage(y_waypoints, x_waypoints, im, color)
 %DrawTrajectoryOnImage Gets the x and y values of the trajectory waypoints
 % and draws it on image. It returns the modified image. If not set, color
-% will be automatically set to red
+% will be automatically set to blue
 if nargin < 4
-    color = [255 255 0];
+    color = [0 0 255];
 end
 drawn_image = im;     % creates a merged image
 N = length(x_waypoints);
