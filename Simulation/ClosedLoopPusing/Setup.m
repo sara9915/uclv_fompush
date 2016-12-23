55a988ecd548524a4f32cabd3f9aada68ad5b70e
eudaldrg <eudald.rg@gmail.com>
2016-12-23 12:33:36 -0500

    Changed Gurobi path to use the environment GUROBI_HOME

--- Simulation/ClosedLoopPusing/Setup.m
+++ Simulation/ClosedLoopPusing/Setup.m
@@ -1,6 +1,7 @@
 addpath('../../software/externals/jsonlab');
+addpath(fullfile('../System'));
 if isunix()
-    addpath('/home/mcube/software/gurobi651/linux64/matlab');
+    addpath(strcat(getenv('GUROBI_HOME'),'/matlab'));
     gurobi_setup;
 elseif ispc()
     addpath('C:\gurobi701\win64\matlab');
