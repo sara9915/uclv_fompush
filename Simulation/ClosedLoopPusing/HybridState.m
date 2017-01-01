8622d4ffadefff6d8ea095ccc51b740b3c99ddc1
eudaldrg <eudald.rg@gmail.com>
2017-01-01 09:31:13 -0500

    Updated the hybrid classes so that the abstract one is generic in dimensions and physics of the problem

--- Simulation/ClosedLoopPusing/HybridState.m
+++ Simulation/ClosedLoopPusing/HybridState.m
@@ -1,84 +1,48 @@
 classdef HybridState < matlab.mixin.Heterogeneous
-    properties (Access = private)
-        % Declare Equilibrium variables
-        ry_star = 0;
-        x_eq = [0;0;0];
-    end
     properties (Access = protected)
         rx;
         rx2;
-        Cbi;
-        gamma_top;
-        gamma_bottom;
-        v_n_star;
-        v_t_star;
-        gamma_top_star; % Part of the c.c. linearization params
-        gamma_bottom_star; % Part of the c.c. linearization params
-        C_top_linear; % Part of the cone constraint(c.c) linearization params
-        C_bottom_linear; % Part of the c.c. linearization params
     end
-    properties (Abstract, Access = protected)
+    properties (Abstract) % Abstract properties cannot define access methods,
+       % they have to be defined in the implementation
        controller_matrix; % Kinematics matrix as a linear combination of the controller inputs
-    end
-    properties (Abstract)
        % The linearization of the dynamics of the system allows us to write
        % the friction cone constraints as: E*x_bar + D*u_bar <= g
-       E;
-       D;
-       g; 
-    end
-    properties
-       A_linear;
-       B_linear;
-       B_nonlinear; % TODO: Change names
-       f_star;
-       u_star = [0.05;0]; % TODO: Change to allow complex trajectories
+       name;
+       x;
+       u;
     end
     
      methods
-         function obj = HybridState(a, nu_p, c2)
-             syms theta ry
-             obj.rx = -a/2.0;
+         function obj = HybridState(a)
+             obj.rx = -a / 2.0;
              obj.rx2 = obj.rx * obj.rx;
-             obj.Cbi = Helper.C3_2d(theta);
-             obj.gamma_top = (nu_p * c2 - obj.rx * ry + nu_p * obj.rx2) / (c2 + ry^2 - nu_p * obj.rx * ry);
-             obj.gamma_bottom = (-nu_p * c2 - obj.rx * ry - nu_p * obj.rx2) / (c2 + ry^2 + nu_p * obj.rx * ry);
          end
-         function obj = SymbolicLinearize(obj)
-            syms x y theta
-            syms ry v_n v_t % ry is py in the paper TODO: Change
+         % TODO: Change the function so that it returns the required
+         % matrices and can be used in more complex trajectories
+         % TODO: When generalizing to more complex cases, the computation
+         % of all the parameters needs to be done at the implementation
+         % classes.
+         function [A, B, B_nonlinear, E, D, g, f_star] ...
+                   = SymbolicLinearize(obj, x_star, u_star) % x = [x, y, theta, ry] u = [v_n, v_t]
             %Build states
-            x_state = [x; y; theta; ry];
-            u_state = [v_n; v_t];
             %Body frame kinematics
-            f = obj.controller_matrix * u_state;
-
+            f = obj.controller_matrix * obj.u.';
             %% Linearization
-            obj.v_n_star = obj.u_star(1);
-            obj.v_t_star = obj.u_star(2);
-            C_top = jacobian(obj.gamma_top,x_state);
-            C_bottom = jacobian(obj.gamma_bottom, x_state);
             %Build jacobians
-            A = jacobian(f, x_state);
-            B = jacobian(f, u_state);
-            obj.B_nonlinear = matlabFunction(simplify(B));
+            A = jacobian(f, obj.x);
+            B = jacobian(f, obj.u);
+            B_nonlinear = matlabFunction(simplify(B));
             % Substitute equilibrium states
-            A = subs(A,{x,y,theta ry},{obj.x_eq(1),obj.x_eq(2),obj.x_eq(3), obj.ry_star});
-            A = subs(A,{v_n,v_t},{obj.v_n_star, obj.v_t_star});
-            B = subs(B,{x,y,theta ry},{obj.x_eq(1),obj.x_eq(2),obj.x_eq(3), obj.ry_star});
-            B = subs(B,{v_n,v_t},{obj.v_n_star, obj.v_t_star});
-    %         Compute LQR solution
-    %         K = lqr(A,B,obj.Q_LQR,obj.R_LQR);
-    %         obj.K = K;  
-            %Set properties
-            obj.A_linear = double(A);
-            obj.B_linear = double(B);
-            obj.C_top_linear = double(subs(C_top, ry, {obj.ry_star}));
-            obj.C_bottom_linear = double(subs(C_bottom, ry, {obj.ry_star}));
-            obj.gamma_top_star = double(subs(obj.gamma_top, ry, obj.ry_star));
-            obj.gamma_bottom_star = double(subs(obj.gamma_bottom, ry, obj.ry_star));
-            obj.f_star = matlabFunction(simplify(f)); % TODO: Check with Frank whether this makes sense
+            subs_x_u = [x_star, u_star];
+            A = double(subs(A, [obj.x, obj.u], subs_x_u));
+            B = double(subs(B, [obj.x, obj.u], subs_x_u));
+            f_star = matlabFunction(simplify(f)); % TODO: Check with Frank why do we simplify and then cast to something again
+            [E, D, g] = obj.GetConstraintMatrices(x_star, u_star);
          end
+     end
+     methods (Abstract)
+         [E, D, g] = GetConstraintMatrices(obj, x_star, u_star);
      end
     
 end
--- Simulation/ClosedLoopPusing/SlidingDownState.m
+++ Simulation/ClosedLoopPusing/SlidingDownState.m
@@ -1,30 +1,37 @@
 classdef SlidingDownState < HybridState
 properties
-    E;
-    D;
-    g;
-end
-properties (Access = protected)
     controller_matrix;
+    name = 'Sliding down';
+    x;
+    u;
+    gamma_bottom;
+    C_bottom;
 end
 properties (Access = private)
     epsilon = 0.005;
 end
 methods
     function obj = SlidingDownState(a, nu_p, c2)
-        obj = obj@HybridState(a, nu_p, c2);
-        syms ry
-        Q = [c2 + obj.rx2 obj.rx * ry;
-             obj.rx * ry c2 + ry^2] ./ (c2 + obj.rx2 + ry^2);
+        obj = obj@HybridState(a);
+        obj.x = sym('x', [1, 4]); % x, y, theta, ry
+        obj.u = sym('u', [1, 2]); % v_n, v_t
+        obj.gamma_bottom = (-nu_p * c2 - obj.rx * obj.x(4) - nu_p * obj.rx2) / (c2 + obj.x(4)^2 + nu_p * obj.rx * obj.x(4));
+        Cbi = Helper.C3_2d(obj.x(3));
+        Q = [c2 + obj.rx2 obj.rx * obj.x(4);
+             obj.rx * obj.x(4) c2 + obj.x(4)^2] ./ (c2 + obj.rx2 + obj.x(4)^2);
         P = [1 0; obj.gamma_bottom 0];
-        b = [(-ry + obj.gamma_bottom * obj.rx) / (c2 + obj.rx2 + ry^2) 0];
+        b = [(-obj.x(4) + obj.gamma_bottom * obj.rx) / (c2 + obj.rx2 + obj.x(4)^2) 0];
         c = [-obj.gamma_bottom 0];
         %Build nonlinear function
-        obj.controller_matrix = [obj.Cbi.' * Q * P; b; c];
-        obj = obj.SymbolicLinearize();
-        obj.E = -obj.v_n_star * obj.C_bottom_linear;
-        obj.D = [-obj.gamma_bottom_star 1];
-        obj.g = [-obj.v_t_star + obj.gamma_bottom_star * obj.v_n_star - obj.epsilon];
+        obj.controller_matrix = [Cbi.' * Q * P; b; c];
+        obj.C_bottom = jacobian(obj.gamma_bottom, obj.x);
+    end
+    function [E, D, g] = GetConstraintMatrices(obj, x_star, u_star)
+        C_bottom_subs = double(subs(obj.C_bottom, obj.x(4), x_star(4)));
+        gamma_bottom_star = double(subs(obj.gamma_bottom, obj.x(4), x_star(4)));
+        E = -u_star(1) * C_bottom_subs;
+        D = [-gamma_bottom_star 1];
+        g = [-u_star(2) + gamma_bottom_star * u_star(1) - obj.epsilon];
     end
 end 
 end
