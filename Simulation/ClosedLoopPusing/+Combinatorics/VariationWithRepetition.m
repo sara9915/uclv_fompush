cc5da128d52f9f6a8b2021a647c4e7c95aa53887
eudaldrg <eudald.rg@gmail.com>
2017-02-08 21:55:06 -0500

    Created a function to test all the modes

---
+++ Simulation/ClosedLoopPusing/+Combinatorics/VariationWithRepetition.m
@@ -0,0 +1,17 @@
+function [ result ] = VariationWithRepetition(n, m)
+%VARIATIONWITHREPETITION Summary of this function goes here
+%   Detailed explanation goes here
+    result = PrivateVariationWithRepetition(n, m, 1, zeros(1, m));
+end
+
+function [result] = PrivateVariationWithRepetition(n, m, i, local)
+    result = [];
+    if i == m + 1
+        result = [result; local];
+    else
+        for j = 1:n
+            local(i) = j;
+            result = [result; PrivateVariationWithRepetition(n, m, i + 1, local)];
+        end
+    end
+end
