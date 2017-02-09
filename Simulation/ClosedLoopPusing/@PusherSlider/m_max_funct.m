e986901a8b5082d9e6f67e42dc592b2caf460945
eudaldrg <eudald.rg@gmail.com>
2017-02-08 22:06:07 -0500

    Removed all the moved files that git didn't detect as moved

--- Simulation/ClosedLoopPusing/@PusherSlider/m_max_funct.m
+++
@@ -1,4 +0,0 @@
-function n_f = m_max_funct(obj, nu, m)     
-    n_f_integrand = @(p1,p2) (nu*m*Helper.g/obj.A) * sqrt([p1;p2;0]'*[p1;p2;0]);
-    n_f = Helper.DoubleGaussQuad(n_f_integrand,-obj.a/2,obj.a/2,-obj.b/2,obj.b/2);
-end
