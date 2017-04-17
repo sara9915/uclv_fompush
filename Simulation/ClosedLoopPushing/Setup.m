5b01497f44d56d7f60868abc84807f2fbcec566c
eudaldrg <eudald.rg@gmail.com>
2017-04-17 13:06:24 -0400

    hardcoded gurobi home

--- Simulation/ClosedLoopPushing/Setup.m
+++ Simulation/ClosedLoopPushing/Setup.m
@@ -2,7 +2,7 @@ addpath('../../software/externals/jsonlab');
 run('../../software/externals/drake/addpath_drake');
 addpath(fullfile('../System'));
 if isunix()
-    addpath(strcat(getenv('GUROBI_HOME'),'/matlab'));
+    addpath('/home/mcube/software/gurobi701/linux64/matlab');
     gurobi_setup;
 elseif ispc()
     addpath('C:\gurobi701\win64\matlab');
